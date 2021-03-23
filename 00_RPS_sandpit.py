342779977406b7425b8eee0aeb255338b6f2a23d
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2021-03-24 09:18:58 +1300

    Up to Game Summary

---
+++ 00_RPS_sandpit.py
@@ -0,0 +1,10 @@
+fruit_list = []
+
+for item in range (0, 4):
+    fruit = input("Fruit: ")
+    fruit_list.append(fruit)
+
+print("the fruit list")
+
+for item in fruit_list:
+    print(item)
---
+++ 07_Game_Summary.py
@@ -0,0 +1,31 @@
+game_summary = []
+
+rounds_played = 5
+rounds_lost = 0
+rounds_tied = 0
+
+for item in range(0,5):
+    result = input("Choose the result: ")
+    history = "Round {}: {}".format(item, result)
+
+    if result == "lost":
+        rounds_lost += 1
+    elif result == "tie":
+        rounds_tied += 1
+
+    game_summary.append(history)
+
+rounds_won = rounds_played - rounds_tied - rounds_lost
+
+win_percent = rounds_won / rounds_played * 100
+loss_percent = rounds_lost / rounds_played * 100
+tie_percent = rounds_tied / rounds_played * 100
+
+print()
+for item in game_summary:
+    print(item)
+print()
+
+print("*** Game Summary ***")
+print("Win: {}, ({:.0f}%) \nLoss: {}, ({:.0f}%) \nTie: {}, ({:.0f}%)"
+      .format(rounds_won, win_percent, rounds_lost, loss_percent, rounds_tied, tie_percent))
